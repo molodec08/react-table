201ce4804ac4912a7e67960988a3bc7928971da9
molodec08 <molodec.08@mail.ru>
2021-03-09 15:29:07 +0300

    правка работы фильтра при изменении пагинации

--- src/component/Select.js
+++ src/component/Select.js
@@ -1,31 +1,32 @@
 import React, {useEffect, useRef} from 'react';
 
 export const Select = (props) => {
-    const selectRef = useRef();
+  const selectRef = useRef();
 
-    useEffect(() => {
-        window.M.FormSelect.init(selectRef.current);
-    }, []);
+  useEffect(() => {
+    window.M.FormSelect.init(selectRef.current);
+  }, []);
 
-    return (
-        <div className="input-field">
-            <select
-                className="icons"
-                ref={selectRef}
-                name={props.name}
-                value={props.value}
-                onChange={props.changeHandler}
-            >
-                <option value="" selected disabled>{props.placeholder}</option>
-                { props.options.map((option) => {
-                    return (
-                        <option
-                            key={option.id}>{option.value}
-                        </option>
-                    )
-                }) }
-            </select>
-            <label htmlFor={props.name}> {props.title} </label>
-        </div>
-    )
+  return (
+    <div className="input-field">
+      <select
+        className="icons"
+        ref={selectRef}
+        name={props.name}
+        value={props.value}
+        defaultValue={'DEFAULT'}
+        onChange={props.changeHandler}
+      >
+        <option value="DEFAULT" disabled>{props.placeholder}</option>
+        { props.options.map((option) => {
+          return (
+            <option
+              key={option.id}>{option.value}
+            </option>
+          )
+        }) }
+      </select>
+      <label htmlFor={props.name}> {props.title} </label>
+    </div>
+  )
 };
